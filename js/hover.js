65af477f8cbc9ce63fc1954621212e5dd11fed3c
kaitlynhova <kaitlynhova@gmail.com>
2015-03-01 22:28:08 -0800

    right hover styles

--- js/hover.js
+++ js/hover.js
@@ -12,4 +12,19 @@ $( document ).ready(function() {
     $("#left-overlay-text").addClass('no-show');
     $('.braintextleft').css('top','-40px');
   });
+
+  $("#rightbox").hover(function(){
+    $("#right-overlay").addClass('right-overlay');
+    $(".right-text").css('display', 'none');
+    $("#right-overlay-text").removeClass('no-show');
+    $('.braintext').css('top','-35px');
+  });
+
+  $("#rightbox").mouseleave(function(){
+    $("#right-overlay").removeClass('right-overlay');
+    $(".right-text").css('display', 'block');
+    $("#right-overlay-text").addClass('no-show');
+    $('.braintext').css('top','-40px');
+  });
+
 });
